3104bbdbc97bbca245e5706c07585cc1c18c10cd
Kur04ka <vanya.kurochkin.23@mail.ru>
2024-09-03 10:51:49 +0700

    add crossplatform support for clearing terminal

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"Chiffre_de_Vigenere/internal/dialog"
-	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
+var clearOS map[string]func()
+
 func main() {
-	fmt.Print("\033[H\033[2J")
+	clearTerminal()
 
 	dialog.Dialog()
+}
+
+func clearTerminal() {
+	if value, ok := clearOS[runtime.GOOS]; ok {
+		value()
+	}
+}
+
+func init() {
+	clearOS = make(map[string]func())
+	clearOS["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clearOS["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clearOS["darwin"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
